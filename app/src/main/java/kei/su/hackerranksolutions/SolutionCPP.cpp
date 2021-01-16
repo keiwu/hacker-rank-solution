eedd97632aa61f37f263259ae777d7dea9d2bac8
kei <keiwus@gmail.com>
2021-01-16 01:09:49 -0800

    merge two sorted link lists and return the head of the merged list This is method 2 of the solution

--- app/src/main/java/kei/su/hackerranksolutions/SolutionCPP.cpp
+++ app/src/main/java/kei/su/hackerranksolutions/SolutionCPP.cpp
@@ -4,6 +4,32 @@
 
 #include "SolutionCPP.h"
 
+//Node **head; //in c++, this meaning pointer to a pointer
+
+int a;                // integer
+int *ptrA = &a        // pointer to a integer  // “&” means “addressof”
+int **PtrPtrA = &ptrA // point to pointer to a integer
+
+How can it be used:
+
+void function_nochange(int *pA  ) {   pA   = &b;  }
+void function_change  (int **ppA) {   *ppA = &b;  }
+
+int a;
+int b;
+void test()
+{
+  int *ptrA = &a
+
+  function_nochange(ptrA)
+  // here ptrA still point to int a since ptrA was copied
+
+  function_change(&ptrA)
+  // here ptrA point to int b since ptrA was passed as pointer
+}
+
+
+
 // Reverse a linked list and return the head of the reversed list
 SinglyLinkedListNode* reverse(SinglyLinkedListNode* head) {
     SinglyLinkedListNode* current = head;
@@ -136,6 +162,56 @@ bool compare_lists(SinglyLinkedListNode* head1, SinglyLinkedListNode* head2) {
     else
         return 0;
 
+}
+
+void moveNode(SinglyLinkedListNode** destPtr, SinglyLinkedListNode** srcPtr){
+    SinglyLinkedListNode* newNode = *srcPtr;
+    //advance the src pointer
+    *srcPtr = newNode->next;
+
+    /* Link the old dest off the new node */
+    newNode->next = *destPtr;
+
+    //move destination to point to the new Node
+    *destPtr = newNode;
+
+
+}
+
+// Complete the mergeLists function below.
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+SinglyLinkedListNode* mergeLists(SinglyLinkedListNode* head1, SinglyLinkedListNode* head2) {
+    SinglyLinkedListNode* result;
+    SinglyLinkedListNode** lastPtrRef = &result;
+
+    while(1){
+        if(head1 == NULL){
+            *lastPtrRef = head2;
+            break;
+        } else if (head2 == NULL){
+            *lastPtrRef = head1;
+            break;
+        }
+
+        if (head1->data <= head2->data){
+            moveNode(lastPtrRef, &head1);
+        } else {
+            moveNode(lastPtrRef, &head2);
+        }
+
+        lastPtrRef = &((*lastPtrRef)->next);
+
+    }
+
+    return result;
 }
 
 
