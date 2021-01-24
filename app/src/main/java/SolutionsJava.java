2e99487736998457bf7f002aaab03022f3b9ef42
kei <keiwus@gmail.com>
2021-01-23 22:57:45 -0800

    // Binary tree preorder traversal using recursive

--- app/src/main/java/SolutionsJava.java
+++ app/src/main/java/SolutionsJava.java
@@ -210,6 +210,34 @@ public class SolutionsJava {
     }
 
 
+    class Node {
+        Node left;
+        Node right;
+        int data;
+
+        Node(int data) {
+            this.data = data;
+            left = null;
+            right = null;
+        }
+    }
+
+    // Binary tree preorder traversal using recursive
+    public static void preOrder(Node root) {
+        while(root!=null){
+            System.out.print(root.data + " ");
+            if(root.left!=null)
+                preOrder(root.left);
+
+            if (root.right!=null)
+                preOrder(root.right);
+
+            break;
+
+        }
+    }
+
+
 
 
 
