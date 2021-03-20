fbd6a1a134df942eddcb57668225206e9afbd60f
kei <keiwus@gmail.com>
2021-03-19 17:27:40 -0700

    Simple Text Editor using Stack. Still fail 5 test cases due to time out

--- app/src/main/java/SolutionsJava.java
+++ app/src/main/java/SolutionsJava.java
@@ -1,5 +1,5 @@
-1
- ,; import java.util.PriorityQueue;
+
+import java.util.PriorityQueue;
 import java.util.Scanner;
 import java.util.Stack;
 
@@ -7,6 +7,137 @@ import java.util.Stack;
 
 import java.io.*;
 import java.util.*;
+
+//Simple Text Editor
+/*
+In this challenge, you must implement a simple text editor. Initially, your editor contains an empty string, . You must perform operations of the following
+
+types:
+
+    append
+
+- Append string to the end of
+.
+delete
+- Delete the last characters of
+.
+print
+- Print the character of
+.
+undo
+- Undo the last (not previously undone) operation of type or , reverting
+
+    to the state it was in prior to that operation.
+
+Input Format
+
+The first line contains an integer,
+, denoting the number of operations.
+Each line of the subsequent lines (where ) defines an operation to be performed. Each operation starts with a single integer, (where ), denoting a type of operation as defined in the Problem Statement above. If the operation requires an argument, is followed by its space-separated argument. For example, if and , line
+
+will be 1 abcd.
+
+Constraints
+
+The sum of the lengths of all in the input
+.
+The sum of
+over all delete operations
+
+    .
+    All input characters are lowercase English letters.
+    It is guaranteed that the sequence of operations given as input is possible to perform.
+
+Output Format
+
+Each operation of type
+must print the
+
+character on a new line.
+
+Sample Input
+
+8
+1 abc
+3 3
+2 3
+1 xy
+3 2
+4
+4
+3 1
+
+Sample Output
+
+c
+y
+a
+
+Explanation
+
+Initially,
+is empty. The following sequence of
+
+operations are described below:
+
+. We append to , so
+.
+Print the
+character on a new line. Currently, the
+character is c.
+Delete the last
+characters in (), so
+.
+Append
+to , so
+.
+Print the
+character on a new line. Currently, the
+character is y.
+Undo the last update to
+, making empty again (i.e.,
+).
+Undo the next to last update to
+(the deletion of the last characters), making
+.
+Print the
+character on a new line. Currently, the character is a.
+ */
+public static void main(String[]args){
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        String str="";
+        Stack<String> stack=new Stack<String>();
+        Scanner sc=new Scanner(System.in);
+        // there are n queryies
+        int n=sc.nextInt();
+        int choice;
+        int k;
+
+        while(n>0){
+            choice=sc.nextInt();
+
+            switch(choice){
+                case 1:
+                    stack.push(str);
+                    str=str+sc.next();
+                    break;
+                case 2:
+                stack.push(str);
+                    k=sc.nextInt();
+                    str=str.substring(0,str.length()-k);
+                    break;
+                case 3:
+                    k=sc.nextInt();
+                    System.out.println(str.charAt(k-1));
+                    break;
+                case 4:
+                    str=stack.peek();
+                    stack.pop();
+                    break;
+            }
+            n--;
+            }
+        }
 
 
 // LargestRectangle using stack
