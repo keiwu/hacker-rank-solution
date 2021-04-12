927c26b4e1b7dd632612e60157e49a076d5654ad
kei <keiwus@gmail.com>
2021-04-12 02:24:50 -0700

    /* Convert a day in 1700 to 2700 to date
    Marie invented a Time Machine and wants to test it by time-traveling to visit Russia on the Day of the Programmer (the 256th day of the year) during a year in the inclusive range from 1700 to 2700.
    
    From 1700 to 1917, Russia's official calendar was the Julian calendar; since 1919 they used the Gregorian calendar system. The transition from the Julian to Gregorian calendar system occurred in 1918, when the next day after January 31st was February 14th. This means that in 1918, February 14th was the 32nd day of the year in Russia.
    
    In both calendar systems, February is the only month with a variable amount of days; it has 29 days during a leap year, and 28 days during all other years. In the Julian calendar, leap years are divisible by 4; in the Gregorian calendar, leap years are either of the following:
    
        Divisible by 400.
        Divisible by 4 and not divisible by 100.
    
    Given a year,
    , find the date of the 256th day of that year according to the official Russian calendar during that year. Then print it in the format dd.mm.yyyy, where dd is the two-digit day, mm is the two-digit month, and yyyy is
    
    .
    
    For example, the given year
    = 1984. 1984 is divisible by 4, so it is a leap year. The 256th day of a leap year after 1918 is September 12, so the answer is 12.09.1984
    
    .
    
    Function Description
    
    Complete the dayOfProgrammer function in the editor below. It should return a string representing the date of the 256th day of the year given.
    
    dayOfProgrammer has the following parameter(s):
    
        year: an integer
    
     */
     */
     */

--- app/src/main/java/SolutionsJava.java
+++ app/src/main/java/SolutionsJava.java
@@ -7,7 +7,93 @@ import java.util.Stack;
 
 import java.io.*;
 import java.util.*;
+/*
+Convert a day in 1700 to 2700 to date
 
+Marie invented a Time Machine and wants to test it by time-traveling to visit Russia on the Day of the Programmer (the 256th day of the year) during a year in the inclusive range from 1700 to 2700.
+
+From 1700 to 1917, Russia's official calendar was the Julian calendar; since 1919 they used the Gregorian calendar system. The transition from the Julian to Gregorian calendar system occurred in 1918, when the next day after January 31st was February 14th. This means that in 1918, February 14th was the 32nd day of the year in Russia.
+
+In both calendar systems, February is the only month with a variable amount of days; it has 29 days during a leap year, and 28 days during all other years. In the Julian calendar, leap years are divisible by 4; in the Gregorian calendar, leap years are either of the following:
+
+    Divisible by 400.
+    Divisible by 4 and not divisible by 100.
+
+Given a year,
+, find the date of the 256th day of that year according to the official Russian calendar during that year. Then print it in the format dd.mm.yyyy, where dd is the two-digit day, mm is the two-digit month, and yyyy is
+
+.
+
+For example, the given year
+= 1984. 1984 is divisible by 4, so it is a leap year. The 256th day of a leap year after 1918 is September 12, so the answer is 12.09.1984
+
+.
+
+Function Description
+
+Complete the dayOfProgrammer function in the editor below. It should return a string representing the date of the 256th day of the year given.
+
+dayOfProgrammer has the following parameter(s):
+
+    year: an integer
+
+ */
+ */
+ */
+public class Solution {
+
+    // Complete the dayOfProgrammer function below.
+    static String dayOfProgrammer(int year) {
+        String date="";
+        int number = 256;
+        int[] days = new int[]{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        if (year<1918){
+            if (year%4==0){
+                days[1] = 29;
+            }
+
+            int i;
+            for(i=0; i<days.length; i++){
+                if (number>days[i]){
+                    number=number-days[i];
+                } else
+                    break;
+            }
+
+            String month;
+            date=number+"."+String.format("%02d", i+1)+"."+year;
+
+        } else if (year==1918){
+            days[1]=15;
+            int i;
+            for(i=0; i<days.length; i++){
+                if (number>days[i]){
+                    number=number-days[i];
+                } else
+                    break;
+            }
+
+            String month;
+            date=number+"."+String.format("%02d", i+1)+"."+year;
+        } else if (year > 1918 && year <=2700){
+            if ((year%4==0&& year%100!=0)|| year%400==0){
+                days[1] = 29;
+            }
+
+            int i;
+            for(i=0; i<days.length; i++){
+                if (number>days[i]){
+                    number=number-days[i];
+                } else
+                    break;
+            }
+            date=number+"."+String.format("%02d",i+1)+"."+year;
+        }
+
+
+        return date;
+
+    }
 
 /*
     Given an array of integers and a positive integer k , determine the number of pairs where i < j and ar[i]+ ar[j] is divisible by .
